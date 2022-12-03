5de59ed3105bb06b8f1acf193ab5a08a26486b6a
natan-ber <118775656+natan-ber@users.noreply.github.com>
2022-12-03 10:52:16 +0100

    Update benchmark_2.jl

--- benchmark_2.jl
+++ benchmark_2.jl
@@ -1,5 +1,5 @@
 using Gridap, GridapGmsh, LinearAlgebra, SparseArrays, Preconditioners, Gridap.Geometry, IterativeSolvers, Random, Test, BenchmarkTools
-model = GmshDiscreteModel("C:/Users/Natan/Documents/CVUT/Athens/Github/bus_door.msh")
+model = GmshDiscreteModel("C:/.../bus_door.msh")
 # writevtk(model,"model")
 
 labels = get_face_labeling(model)
